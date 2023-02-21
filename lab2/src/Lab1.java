d97f79aa6e589ddbf762685fd0a9cecfcd60328f
youngflexxxer <sashagrush12@gmail.com>
2023-02-21 16:38:56 +0300

    fixes

--- lab2/src/Lab1.java
+++ lab2/src/Lab1.java
@@ -1,10 +1,5 @@
 public class Lab1 {
-    public void main(String input) {
-
-    }
-    public double computeArea(Point3d point1, Point3d point2, Point3d point3) {
-        double a = Math.sqrt((point1.getX() - point2.getX()) * (x1 - x2) + (y1 - y2) * (y1 - y2));
-        double b = Math.sqrt((x1 - x3) * (x1 - x3) + (y1 - y3) * (y1 - y3));
-        double c = Math.sqrt((x2 - x3) * (x2 - x3) + (y2 - y3) * (y2 - y3));
+    public static void main(String[] args) {
+        Point3d point  = new Point3d(1,2,3);
     }
 }
