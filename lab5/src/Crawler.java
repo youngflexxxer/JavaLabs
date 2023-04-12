2653ac1638b58633eb2cf7091611784ecbb56e79
youngflexxxer <sashagrush12@gmail.com>
2023-04-12 18:26:00 +0300

    attemp to lab5

--- lab5/src/Crawler.java
+++ lab5/src/Crawler.java
@@ -1,8 +1,28 @@
-public class Crawler {
-    public static final String URL_PREFIX = "http://";    
-    public static void main(String[] args) {
-        if (args.length != 2) {
+import java.net.*;
+import java.util.LinkedList;
+import java.io.*;
 
+public class Crawler {
+    public static final String URL_PREFIX = "http://";
+    public static final int port = 80;    
+
+    LinkedList <URLDepthPair> unprocessedUrlDepthPairs = new LinkedList <URLDepthPair> ();
+    LinkedList <URLDepthPair> processedUrlDepthPairs = new LinkedList <URLDepthPair> ();
+
+    public static void main(String[] args) throws NumberFormatException, UnknownHostException, IOException {
+        if (args.length == 2) {
+            scan(args[0], Integer.parseInt(args[1]));
         }
+    }
+    public static void scan(String URL, int depth) throws UnknownHostException, IOException {
+        Socket socket = new Socket(URL, port);
+        socket.setSoTimeout(10000);
+        InputStreamReader in = new InputStreamReader (socket.getInputStream());
+        BufferedReader reader = new BufferedReader(in);
+        String line = reader.readLine();
+        while(line != null) {
+            System.out.println(line);
+        }
+        socket.close();
     }
 }
---
+++ lab5/src/URLDepthPair.java
@@ -0,0 +1,9 @@
+public class URLDepthPair {
+    private String URLAddress;
+    private int depth;
+
+    @Override
+    public String toString() {
+        return URLAddress + " " + depth;
+    }
+}
